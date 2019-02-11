78d36d849b441ad86eb3615bac981a82a2935f91
Salaheldin Akl <salaheldin.akl@student.dit.ie>
2019-02-11 13:40:00 +0000

    Add worksheet solution

---
+++ worksheets/1-rest-sql-orm/lab1-2/initApp.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 5005
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next()
+})
+
+module.exports = {
+    app,
+    port,
+}
---
+++ worksheets/1-rest-sql-orm/lab1-2/initDB.js
@@ -0,0 +1,41 @@
+const Seq = require('sequelize')
+
+const sequelize = new Seq({
+    username: 'postgres',
+    password: 'toor',
+    database: 'pgguide',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    define: {
+        underscored: true,
+        timestamps: false,
+    }
+})
+
+const Product = sequelize.define('product', {
+    id: {
+        type: Seq.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    title: {
+        type: Seq.STRING,
+    },
+    price: {
+        type: Seq.NUMERIC,
+    },
+    created_at: {
+        type: Seq.DATE,
+    },
+    deleted_at: {
+        type: Seq.DATE,
+    },
+    tags: {
+        type: Seq.ARRAY(Seq.STRING),
+    }
+})
+
+module.exports = {
+    sequelize,
+    Product,
+}
