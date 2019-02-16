02a49e938d68c46544b39b1e94d24174149cee31
Simon-Unsworth <simonunsworth@hotmail.com>
2019-02-16 17:17:05 +0000

    users endpoint working correctly

---
+++ worksheets/1-rest-sql-orm/massive_implementation.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const http = require('http');
+const massive = require('massive');
+
+const app = express();
+
+massive({
+    host: '127.0.0.1',
+    port: 5432,
+    database: 'pgguide',
+    user: 'postgres',
+    password: ''
+}).then(instance => {
+
+    app.set('db', instance);
+
+    // endpoint for users email and sex
+    app.get('/users', (req, res) => {
+        req.app.get('db').users.find(
+            {/* find all */},
+            {
+                fields: ['email', 'created_at'],
+                exprs: {
+                    sex: "details::json->>'sex'"
+                },
+                order: [{
+                    field: 'created_at',
+                    direction: 'desc'
+                }]
+            }
+        ).then(items => {
+            res.json(items);
+        });
+    });
+
+    app.get('/products', (req, res) => {
+        req.app.get('db').products.find({
+        }).then(items => {
+            res.json(items);
+        });
+    });
+
+    app.get('/products:id', (req, res) => {
+        req.app.get('db').products.findOne(
+
+        ).then(items => {
+            res.json(items);
+        });
+    });
+
+    app.get('/purchases', (req, res) => {
+        req.app.get('db').purchases.find({
+        }).then(items => {
+            res.json(items);
+        });
+    });
+
+    http.createServer(app).listen(3000);
+
+});
