13e691a60cb0f315ca50d12043c7a7e0140a35d4
Mark_Waldorn <C15501723@mydit.ie>
2019-02-17 22:54:38 +0000

    Added week 2

--- Sequlize/index.js
+++ Sequlize/index.js
@@ -1,15 +1,20 @@
-const Sequelize = require('sequelize');
+
 const express = require('express');
+const sequelize = require('sequelize');
 const app = express();
-app.use(express.urlencoded())
+const db = new sequelize('postgres://postgres:password@127.0.0.1:5432/pgguide');
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+//check connection to postgres database
+db.authenticate().then(() => {
+	console.log('Connection to database successful! :)');
+}).catch(err => {
+	console.log('Unable to connect to the database :( '+err);
+})
 const port = 3000;
 
+app.listen(port, () => console.log(`Express, listening on port ${port}!`));
 
 
-const sequelize = new Sequelize('pgguide', 'postgres', 'Waldo1997', {
-    host: "localhost",
-    dialect: 'postgres',
-    define: {
-        timestamps: false
-    }
-});
--- index.js
+++ index.js
@@ -15,7 +15,7 @@ massive({
   port: 5432,
   database: 'pgguide',
   user: 'postgres',
-  password: 'password',//Put in password
+  password: '',//Put in password
 }).then(instance => {
     app.set('db', instance);
 
@@ -24,15 +24,13 @@ massive({
     {
         app.get('db').users.find(
         {
-
-
         },
         {
             order: [{field: 'created_at', direction: 'desc'}],
             exprs: {
-                email: 'email'
-              },
-            
+                email: 'email',
+                gender: "details::json->>'sex'",
+            },    
         }
         ).then(users => {
         res.json(users);
@@ -43,113 +41,79 @@ massive({
     {
         app.get('db').users.find(
         {
-
             id: 4  
-
         },
         {
             order: [{field: 'created_at', direction: 'desc'}],
             exprs: {
-                email: 'email'
+                email: 'email',
+                gender: "details::json->>'sex'",
               },
-            
         }
         ).then(users => {
         res.json(users);
       });
     });
-
-
-    /*Products 
-    app.get('/products', (req, res) => 
-    {
-        app.get('db').products.find(
-        {
-
-
-
-        },
-        {
-            
-            
-        }
-        ).then(users => {
-        res.json(users);
-      });
-    });*/
-    
 
     app.get('/purchases', (req, res) => 
     {
         app.get('db').products.find(
         {
-
-            
-
         },
-        {
-            
-            
+        {   
         }
         ).then(users => {
         res.json(users);
       });
     });
 
-    /*.then(db => {
-        
-        
-        //Part 2 string localhost:3000/products"/ update products where title = "dictionary";--
-        
-        app.get('/products', (req, res) => db.query("SELECT * FROM products").then(res =>{
-            
-            console.log(res)
-        }
-        ))*/
-
-    
-		
-    
-});
-
-
-
-    //part 2 used for the sql injection 
-    //string used localhost:3000/products/Dictionary'; DELETE FROM users WHERE id=1;
-    /*
-    then(db => {
-        
-        app.get('/products', (req, res) => db.query("SELECT * FROM products").then(res =>{
-            
-            console.log(res)
-        }
-        ))
-        */
-        
-    //Part 3
-    app.get('/products', (req, res) => {
-     //SAFE ALTERNATIVE USING PARAMETERISED QUERY    
-    if(req.query.name != null)
+    app.get('/products', (req, res) => 
     {
-        app.get("db").products.find({or:[{'title like':"%"+req.query.name+"%"}]}, {
-        order:[{field: "price",
-        direction: "asc"}]
-    })
-    .then(result => {res.json(result);})
+        app.get('db').products.find(
+        {
+        },
+        {   
+        }
+        ).then(users => {
+        res.json(users);
+      });
+    });
 
-    /*SAFE ALTERNATIVE USING STORED PROCEDURE
-		app.get("db").get_products_by_title(req.query.name)
-		.then(result => {res.json(result);})*/
-    }
-    else{
+    /*
+    //Part 2 testing localhost:3000/products/Dictionary';UPDATE products SET price='93039' WHERE title='Dictionary';-- 
+    app.get('/products/:title', (req,res) => {
+        var query = req.params.title;
+        app.get('db').query("SELECT * FROM products WHERE title= '"+query+"'").then(products=> {
+            res.send(products);
+        });
+    });*/
         
-        app.get("db").products.find({}, {
-            order:[{field: "price",
-            direction: "asc"}]
-        })
-        .then(result => {res.json(result);})
-    }
+
+    /*Attempt at part 3
+    app.get('/products', (req, res) => 
+    {
+        //SAFE ALTERNATIVE USING PARAMETERISED QUERY    
+       if(req.query.name != null)
+       {
+           app.get("db").products.find({or:[{'title like':"%"+req.query.name+"%"}]}, {
+           order:[{field: "price",
+           direction: "asc"}]
+       })
+       .then(result => {res.json(result);})
+       }
+       else{
+           
+           app.get("db").products.find({}, {
+               order:[{field: "price",
+               direction: "asc"}]
+           })
+           .then(result => {res.json(result);})
+       }
+    */
 });
+
+   
+    
 
 
 
@@ -166,7 +130,5 @@ massive({
         console.log(res)
     }
     ))
-
+*/
    
-
-});*/
