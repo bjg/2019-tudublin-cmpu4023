9877481911d190c6ddf5b7f5d250abbe73d5fcdf
Ronan <connollyro@gmail.com>
2019-02-18 13:43:59 +0000

    bear token check working

--- worksheets/2-authentication/index.js
+++ worksheets/2-authentication/index.js
@@ -21,12 +21,12 @@ massive({
 
     app.get('/', (req, res) => {
       res.json({
-        message: 'Hello World'
+        message: Math.floor(Date.now() / 1000) + ((60 * 60) * 24)
       });
     });
     
     
-    app.get('/products', (req, res) => {
+    app.post('/products', checkToken,(req, res) => {
       db.products.find({},{}).then(products => {
         res.json(products);
       })
@@ -49,8 +49,14 @@ massive({
               "message": "not authenticated"
             })
           }else{
-            res.json({
-              "message": "Success! authenticated"
+            jwt.sign(
+              {
+                user:req.body.username,
+                exp: Math.floor(Date.now() / 1000) + ((60 * 60) * 24)
+              },'secretkey',(error,token) =>{
+              res.json({
+                token: token
+              })
             })
           }
         }).catch(error =>{
@@ -58,6 +64,21 @@ massive({
         });
         
     });
+
+    function checkToken(req,res,next){
+      //authentication header val
+      const bearer_header = req.headers['authorization'];
+      if(typeof bearer_header !== 'undefined'){
+        const bearer_array = bearer_header.split(" ");
+        const bearer_token = bearer_array[1];  
+        req.token = bearer_token;
+        next();   
+      }
+      else{
+        res.sendStatus(403);
+      }
+
+    }
     
     
     app.get('/testdb', (req, res) => {
