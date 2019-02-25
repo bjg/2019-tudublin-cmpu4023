a13ace43c64c8c58530bc7b45e48a1afca25e1c3
Simon-Unsworth <simonunsworth@hotmail.com>
2019-02-25 10:52:04 +0000

    minor change

--- worksheets/2-authentication/app.js
+++ worksheets/2-authentication/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+var bodyParser = require('body-parser');
 const massive = require('massive');
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 massive({
     host: '127.0.0.1',
@@ -15,8 +19,8 @@ massive({
         res.send('Hello World!');
     });
 
-    app.get('/api/authenticate', (req, res) => {
-        res.send('Hello World!');
+    app.post('/api/authenticate', (req, res) => {
+        res.send(req.body.username + ' ' + req.body.password);
     });
 
     app.get('/api/products', (req, res) => {
