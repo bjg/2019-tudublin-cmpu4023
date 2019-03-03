9dfd1e3aabdf721ffe109d37239e790d568e65b0
Eric Strong <c15708709@mydit.ie>
2019-03-03 20:04:20 +0000

    create app tutorial

--- worksheets/3-graphql/prisma-hello-world/index.js
+++ worksheets/3-graphql/prisma-hello-world/index.js
@@ -1,84 +1,69 @@
-// const { prisma } = require('./generated/prisma-client')
-
-// // A `main` function so that we can use async/await
-// async function main() {
-
-//   // Create a new user called `Alice`
-//   const newUser = await prisma.createUser({ name: 'Alice' })
-//   console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
-
-//   // Read all users from the database and print them to the console
-//   const allUsers = await prisma.users()
-//   console.log(allUsers)
-
-//   //fetch list
-//   const usersCalledAlice = await prisma
-//   .users({
-//     where: {
-//       name: 'Alice'
-//     }
-//   })
-//   console.log('-----------' + usersCalledAlice);
-
-//   //update users
-//   const updatedUser = await prisma
-//   .updateUser({
-//     where: { id: 'cjst9lekp002s0a92a9w1p3r0' },
-//     data: { name: 'Bob' }
-//   })
-//   console.log('-------------' + updatedUser)
-
-
-//   //delete user
-//   const deletedUser = await prisma
-//   .deleteUser({ id: 'cjst9n8v9003d0a92u8s3nn8f' })
-
-// }
-
-
-// main().catch(e => console.error(e))
-
-// const { prisma } = require('./generated/prisma-client')
-
-// // A `main` function so that we can use async/await
-// async function main() {
-
-//   // Create a new user with a new post
-//   const newUser = await prisma
-//     .createUser({
-//       name: "Bob",
-//       email: "bob@prisma.io",
-//       posts: {
-//         create: [{
-//           title: "Join us for GraphQL Conf in 2019",
-//         }, {
-//           title: "Subscribe to GraphQL Weekly for GraphQL news",
-//         }]
-//       },
-//     })
-//   console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
-
-//   // Read all users from the database and print them to the console
-//   const allUsers = await prisma.users()
-//   console.log(allUsers)
-
-//   const allPosts = await prisma.posts()
-//   console.log(allPosts)
-// }
-
-// main().catch(e => console.error(e))
-
 const { prisma } = require('./generated/prisma-client')
+const { GraphQLServer } = require('graphql-yoga')
 
-// A `main` function so that we can use async/await
-async function main() {
+const resolvers = {
+  Query: {
+    publishedPosts(root, args, context) {
+      return context.prisma.posts({ where: { published: true } })
+    },
+    post(root, args, context) {
+      return context.prisma.post({ id: args.postId })
+    },
+    postsByUser(root, args, context) {
+      return context.prisma.user({
+        id: args.userId
+      }).posts()
+    }
+  },
+  Mutation: {
+    createDraft(root, args, context) {
+      return context.prisma.createPost(
+        {
+          title: args.title,
+          author: {
+            connect: { id: args.userId }
+          }
+        },
 
-  // Read the previously created user from the database and print their posts to the console
-  const postsByUser = await prisma
-    .user({ email: "bob@prisma.io" })
-    .posts()
-  console.log(`All posts by that user: ${JSON.stringify(postsByUser)}`)
+      )
+    },
+    publish(root, args, context) {
+      return context.prisma.updatePost(
+        {
+          where: { id: args.postId },
+          data: { published: true },
+        },
 
+      )
+    },
+    createUser(root, args, context) {
+      return context.prisma.createUser(
+        { name: args.name },
+      )
+    }
+  },
+  User: {
+    posts(root, args, context) {
+      return context.prisma.user({
+        id: root.id
+      }).posts()
+    }
+  },
+  Post: {
+    author(root, args, context) {
+      return context.prisma.post({
+        id: root.id
+      }).author()
+    }
+  }
 }
 
-main().catch(e => console.error(e))
+//graphQL server
+const server = new GraphQLServer({
+  typeDefs: './schema.graphql',
+  resolvers,
+  context: {
+    prisma
+  },
+})
+server.start(() => console.log('Server is running on http://localhost:4000'))
