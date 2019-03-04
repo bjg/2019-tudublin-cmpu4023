733ccbe82c24bd734df107e8513e4aa06745da28
Colm O'Reilly <c15502723@mydit.ie>
2019-03-04 15:56:13 +0000

    lab complete

--- worksheets/2-authentication/Part 4/hmac.js
+++ worksheets/2-authentication/Part 4/hmac.js
@@ -1,46 +1,75 @@
 const express = require('express');
-const bodyParser = require('bpdy-parser');
+const bodyParser = require('body-parser');
 const massive = require('massive');
-const crypto = require('crypto');
-
+const crypto = require('crypto-js');
+const path = require("path");
+const ejs = require("ejs");
 const app = express();
-const port = 3000;
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extends:true}));
 app.use(bodyParser.json());
 
-massive({
+const configuration = {
     host: '127.0.0.1',
-    port: 5432,
-    database: 'kart',
-    user: 'postgres',
-    password: '8myd!nner',
-    ssl: false,
-    poolSize: 10
-}).then(instance => {
-    app.set('db', instance);
+        port: 5432,
+        database: 'kart',
+        user: 'postgres',
+        password: '8myd!nner',
+        ssl: false
+};
 
-    app.post('/api/products',(req,res) => {
-    const ACCESS_KEY = 'uj&7AK6^A#e|R,Vn[m$A'
-    const SECRET_KEY = 'uj&7AK6^A#e|R,Vn[m$A=^B;aaAb@{c]3@n=pnZW'
+//Authenticate HMAC token
+const hmacAuthentication = (req, res, next)=>{
+    //Get header
+    const authorizationHeader = req.headers.authorization;
+    //split or parse header into array
+    const authArray = authorizationHeader.split(' ');
+    //Get HMAC token
+    const hmac = authArray[1];
+    //Get access key
+    const access_key = authArray[3].slice(0,-1);
+    //Get message from the body
+    const message = req.body.message;
+    //Debug log
+    console.log(`Message ${message}`);
+    console.log(`Access Key ${access_key}`);
+    console.log(`The extracted hmac is ${hmac}`);
 
-    const hash = crypto.createHmac('sha256', ACCESS_KEY).update(SECRET_KEY).digest('hex')
-
-    if(hash === req.headers['authorization']) {
-            
-        req.app.get('db').query (
-            "SELECT * FROM \"Products\""
-        ).then(products => {
-            res.json(products);
-        })
-
+    //Get secret key from db and create new hmac
+    //compare new hmac to old 
+    massive(configuration).then(db => {
+        db.query(`Select secret_key from users_extended where access_key = $1;`, [access_key])
+    .then(record => {
+    console.log(record);
+    const secret_key = Buffer.from(record[0].secret_key).toString('ascii');
+    console.log(secret_key);
+    const confirmHMAC = `${message}${access_key}`;
+    let newHMAC = crypto.HmacSHA256(confirmHMAC, secret_key);
+    newHMAC = Buffer.from(newHMAC.toString()).toString('base64');
+    console.log(`HMACS __ ${newHMAC}`);
+    if (newHMAC === hmac) {
+        next();
     } else {
-        res.status(403).json({
-            Status: 'Forbidden'
-        });
+        res.sendStatus(401);
+        Status: 'Forbidden'
     }
+    }).catch(err => console.log(err));
+});
+next();
+    
+};
+
+//index route
+app.get('/',(req,res,next)=> {
+    res.render('client');
+});
+
+//Protected route requiring HMAC authentication
+app.post("/authenticateMessage", hmacAuthentication, (req, res, next) => {
+    res.sendStatus(200);
 });
 
 app.listen(3000,() => console.log('Server started on port 3000'));
 
-});
--- worksheets/2-authentication/kart.sql
+++ worksheets/2-authentication/kart.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS users_extended;
 
 CREATE TABLE users(
     username varchar(250) NOT NULL,
@@ -11,6 +12,13 @@ CREATE TABLE products(
     productname varchar(30),
     unitprice int,
     unitstock int
+);
+
+CREATE TABLE users_extended(
+    username varchar(250) NOT NULL,
+    password varchar(250) NOT NULL,
+    access_key BYTEA, 
+    secret_key BYTEA
 );
 
 INSERT INTO users(username,password) 
@@ -31,3 +39,6 @@ INSERT INTO products(productid,productname, unitprice, unitstock)
                     (3, 'Laptop', 986, 26),
                     (4, 'Mouse', 13, 4),
                     (5, 'Bottle', 100, 3);
+
+INSERT INTO users_extended(username, password, access_key, secret_key)
+VALUES ('Tom', crypt('H3llo',gen_salt('bf')), '63270a5d4770a09a139bcc4b120ccf60e1cc8826','5812483e092c28e4d55e903deee1ecd420c7de75905e9699344cebb075ac530abe991d6f8605443ace5e26d64c');
