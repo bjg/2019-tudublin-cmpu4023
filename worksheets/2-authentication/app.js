7bba6f382adc36d654530d9093fbd9906eeaebb7
AodhanBrown <c15346551@mydit.ie>
2019-03-06 13:24:29 +0000

    JWT and HMAC working with mock table data

--- worksheets/2-authentication/app.js
+++ worksheets/2-authentication/app.js
@@ -8,6 +8,7 @@ const bp = require('body-parser')
 const massive = require('massive')
 const monitor = require('pg-monitor')
 
+app.use(bp.json())
 
 // connection string
 /*
@@ -47,7 +48,8 @@ app.post('/api/login', (req, res) => {
     //mock user for testing
     const user ={
         id: 1,
-        username: 'c15346551'
+        username: 'c15346551',
+        password: '$2a$06$xoI.he3ymnFljF4.k8e9.ODI0U6OXn/fV1zaFCIy5qIpR555e6Ida'
     }
     jwt.sign({user}, 'secretkey', { expiresIn: '24h' }, (err,token) =>{
         res.json({
@@ -79,6 +81,46 @@ function verifyToken(req, res, next){
         res.sendStatus(403);
     }
 }
+
+
+// HMAC
+
+app.get('/api/select_user', (req, res) => {
+
+    //mock user for testing
+    const user ={
+        id: 1,
+        username: 'c15346551',
+        password: '$2a$06$xoI.he3ymnFljF4.k8e9.ODI0U6OXn/fV1zaFCIy5qIpR555e6Ida',
+        u_secretkey: 'L76RN91LTS98RA47H3ILWL3NWW62RQC5S57U1KDU6V5DH7GGTX4ZZMS8Y2WR7IOSM3I3DSXA2QU9YTPI',
+        u_accesskey: '69LS8SQUXS7KTFLA7MS2PJY2RWC51D7VF3WDCDGF'
+    }
+
+    const request_HMAC = req.headers.authorization
+    const accesskey = req.body.accesskey
+    const username  = req.body.username
+    const messagebody = accesskey + username
+    
+    if(user.username == username){
+
+        const HMAC = crypto.createHmac('sha256', user.u_secretkey)
+        HMAC.update(messagebody)
+        const true_HMAC = HMAC.digest('hex')
+
+        if(true_HMAC == request_HMAC){
+            res.json({
+                user,
+                message: 'Autherization successful'
+            });
+        }else{
+            res.sendStatus(403);
+        }
+
+    }else{
+        res.sendStatus(401);
+    }
+    
+});
 
 app.listen(5000, () => console.log('server has started on port 5000'));
     //};
