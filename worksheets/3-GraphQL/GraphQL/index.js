b64e8a2dbe5ed224fa17c162b8683a737e960aba
SemajDraw <32390574+SemajDraw@users.noreply.github.com>
2019-03-11 17:07:10 +0000

    Change datamodel tutorial

--- worksheets/3-GraphQL/GraphQL/index.js
+++ worksheets/3-GraphQL/GraphQL/index.js
@@ -7,13 +7,38 @@ async function main() {
   const newUser = await prisma.createUser({ name: 'Alice' })
   console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
 
+  // Create a new user with a new post
+  const newUserPost = await prisma
+    .createUser({
+      name: "Bob",
+      email: "brian@prisma.io",
+      posts: {
+        create: [{
+          title: "Join us for GraphQL Conf in 2019",
+        }, {
+          title: "Subscribe to GraphQL Weekly for GraphQL news",
+        }]
+      },
+    })
+  console.log(`Created new user: ${newUserPost.name} (ID: ${newUserPost.id})`)
+
+  // Display all posts
+  const allPosts = await prisma.posts()
+  console.log(allPosts)
+
+  // Read the previously created user from the database and print their posts to the console
+  const postsByUser = await prisma
+    .user({ email: "bob@prisma.io" })
+    .posts()
+  console.log(`All posts by that user: ${JSON.stringify(postsByUser)}`)
+
   // Read all users from the database and print them to the console
   const allUsers = await prisma.users()
   console.log(allUsers)
 
   // Fetch a single user
   const user = await prisma
-  .user({ id: 'cjt4gja74000h0703jmreyal3' })
+  .user({ id: 'cjt4gk6oq000n0703nlcs3g7k' })
 
   // Filtered user list
   const usersCalledAlice = await prisma
@@ -26,13 +51,13 @@ async function main() {
   // Update a users name
   const updatedUser = await prisma
   .updateUser({
-    where: { id: 'cjt4gja74000h0703jmreyal3' },
+    where: { id: 'cjt4gk6oq000n0703nlcs3g7k' },
     data: { name: 'Bob' }
   })
 
   // Delete a user
-  const deletedUser = await prisma
-  .deleteUser({ id: 'cjt4gja74000h0703jmreyal3' })
+//   const deletedUser = await prisma
+//   .deleteUser({ id: 'cjt4gja74000h0703jmreyal3' })
 }
 
 main().catch(e => console.error(e))
