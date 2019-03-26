dbee1ff2091b9efa71df5db18a7959c9f4038853
David O'Neill <doneill39@gmail.com>
2019-03-26 13:41:52 +0000

    Hellow-World, Q1 and Q2 complete.

--- eadlab3-GraphQL/GraphQLab/src/index.js
+++ eadlab3-GraphQL/GraphQLab/src/index.js
@@ -1,33 +1,22 @@
 const { GraphQLServer } = require('graphql-yoga')
-const { prisma } = require('../generated/prisma-client')
-
-const typeDefs = `
-type Query { 
-    category: [Categories!]!
-    title: String! 
-}
-
-type Categories {
-    category: ID!
-    categoryname: String!
-}
-`
-
-let categories = [{
-    category: '20',
-    categoryname: 'Action',
-}]
+const { prisma } = require('../prisma/generated/prisma-client/')
 
 const resolvers = {
     Query: {
-        prod_id: () => `15`,
-        // 2
-        category: () => categories,
-    },
-    // 3
-    Categories: {
-        category: (parent) => parent.category,
-        categoryname: (parent) => parent.categoryname
+        getProducts(root, args, context) {
+            return context.prisma.productses()
+        },
+        getReorder(root, args, context) {
+            return context.prisma.reorders()
+        },
+
+        getInventory(root, args, context) {
+            return context.prisma.inventories()
+        },
+
+        getCategories(root, args, context) {
+            return context.prisma.categorieses()
+        }
     }
 }
 
