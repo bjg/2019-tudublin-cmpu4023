fffa04bfa544fc467f79a250542f134d3ef2eff1
tasyaoc95 <c13365696@mydit.ie>
2019-03-26 18:08:20 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,190 +1,45 @@
-const {
-  GraphQLServer 
-} = require('graphql-yoga')
-const { 
-  prisma 
-} = require('./generated/prisma-client')
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
 
-const resolvers = {
-  Query: {
-    //Part Two. Any of the following queries should suffice.
-    allCategories: (root, args, context, info) => {
-      return context.prisma.categories()
-    },
-    allProducts: (root, args, context, info) => {
-      return context.prisma.products()
-    },
-    allInventories: (root, args, context, info) => {
-      return context.prisma.inventories()
-    },
-    allReorders: (root, args, context, info) => {
-      return context.prisma.reorders()
-    },
-    allCustomers: (root, args, context, info) => {
-      return context.prisma.customers()
-    },
-    allOrders: (root, args, context, info) => {
-      return context.prisma.orders()
-    },
-    //Part Three. This query returns Orderline information, product information,
-    //and the product category. It can be used to see product details for each
-    //line of an order. The lineDetails resolver performs the same function but
-    //an ID is used to specify the order line. 
-    allOrderlines: (root, args, context, info) => {
-      return context.prisma.orderlines()
-    },
-    allCust_hist: (root, args, context, info) => {
-      return context.prisma.cust_hists()
-    },
-    lineDetails: (root, args, context, info) => {
-      return context.prisma.orderlines({id: args.id})
-    },
+var models = [
+  {
+    name: "Category",
+    embedded: false
   },
-  Mutation: {
-    createCategories: (root, args, context) => {
-      return context.prisma.createCategory({
-	categoryname: args.categoryname,
-      })
-    },
-    createProducts: (root, args, context) => {
-      return context.prisma.createProduct({
-        title: args.title,
-	actor: args.actor,
-	price: args.price,
-	special: args.special,
-        common_prod_id: args.common_prod_id,
-	category: {
-	  connect: {
-	    id: args.categoryId
-	  }
-        }
-      })
-    },
-    createInventories: (root, args, context) => {
-      return context.prisma.createInventory({
-	quan_in_stock: args.quan_in_stock,
-	sales: args.sales,
-	product: {
-	  connect: {
-	    id: args.productId
-	  }
-	}
-      })
-    },
-    createReorders: (root, args, context) => {
-      return context.prisma.createReorder({
-	date_low: args.date_low,
-	quan_low: args.quan_low,
-	date_reordered: args.date_reordered,
-	quan_reordered: args.quan_reordered,
-	date_expected: args.date_expected,
-	product: {
-	  connect: {
-	    id: args.productId
-	  }
-	}
-      })
-    },
-    createCustomers: (root, args, context) => {
-      return context.prisma.createCustomer({
-        firstname: args.firstname,
-        lastname: args.lastname,
-        address1: args.address1,
-        address2: args.address2,
-        city: args.city,
-        state: args.state,
-        zip: args.zip,
-        country: args.country,
-        region: args.region,
-        email: args.email,
-        phone: args.phone,
-        creditcardtype: args.creditcardtype,
-        creditcard: args.creditcard,
-        creditcardexpiration: args.creditcardexpiration,
-        username: args.username,
-        password: args.password,
-        age: args.age,
-        income: args.income,
-        gender: args.gender
-      })
-    },
-    //This mutation can be used to create an order and add the relation to
-    //customer table.
-    createOrders: (root, args, context) => {
-      return context.prisma.createOrder({
-	orderdate: args.orderdate,
-	netamount: args.netamount,
-	tax: args.tax,
-	totalamount: args.totalamount,
-        customer: {
-	  connect: {
-	    id: args.customerId
-	  }
-	}
-      })
-    },
-    createOrderlines: (root, args, context) => {
-      return context.prisma.createOrderline({
-        quantity: args.quantity,
-	orderdate: args.orderdate,
-	product: {
-	  connect: {
-	    id: args.productId
-	  }
-	},
-	order: {
-	  connect: {
-	    id: args.orderId
-	  }
-	}
-      })
-    },
+  {
+    name: "Product",
+    embedded: false
   },
-  Product: {
-    category(root, args, context)  {
-      return context.prisma.product({
-	id: root.id
-      }).category()
-    },
+  {
+    name: "Inventory",
+    embedded: false
   },
-  Inventory: {
-    product(root, args, context)  {
-      return context.prisma.inventory({
-        id: root.id
-      }).product()
-    },
+  {
+    name: "Reorder",
+    embedded: false
   },
-  Reorder: {
-    product(root, args, context)  {
-      return context.prisma.reorder({
-	id: root.id
-      }).product()
-    },
+  {
+    name: "Customer",
+    embedded: false
   },
-  Order: {
-    customer(root, args, context) {
-      return context.prisma.order({
-	id: root.id
-      }).customer()
-    },
+  {
+    name: "Order",
+    embedded: false
   },
-  Orderline: {
-    order(root, args, context) {
-      return context.prisma.orderline({
-	id:root.id
-      }).order()
-    },
-    product(root, args, context) {
-      return context.prisma.orderline({
-	id:root.id
-      }).product()
-    },
+  {
+    name: "Orderline",
+    embedded: false
   },
-}
-
-const server = new GraphQLServer({
-  typeDefs: './schema.graphql',
-  resolvers,
-  context: { prisma },
-})
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+  {
+    name: "Cust_hist",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `http://localhost:4466`
+});
+exports.prisma = new exports.Prisma();
