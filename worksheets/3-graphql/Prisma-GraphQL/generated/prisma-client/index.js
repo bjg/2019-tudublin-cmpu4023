207fc999862e7ae9e04d127d51baf8275d527e83
GabrielGrimberg <gabby.gr97@hotmail.com>
2019-03-27 21:42:05 +0000

    Lab 3 Submission - C15478448 - Worksheet 3

---
+++ worksheets/3-graphql/Prisma-GraphQL/generated/prisma-client/index.js
@@ -0,0 +1,45 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Customers",
+    embedded: false
+  },
+  {
+    name: "Orders",
+    embedded: false
+  },
+  {
+    name: "Categories",
+    embedded: false
+  },
+  {
+    name: "Products",
+    embedded: false
+  },
+  {
+    name: "Reorder",
+    embedded: false
+  },
+  {
+    name: "Inventory",
+    embedded: false
+  },
+  {
+    name: "Orderlines",
+    embedded: false
+  },
+  {
+    name: "Cust_hist",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `http://localhost:4466`
+});
+exports.prisma = new exports.Prisma();
