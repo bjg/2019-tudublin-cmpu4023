ea09c33b40ff990adc2d3c18134be78d1add5644
patrickmalonework@gmail.com <patrickmalonework@gmail.com>
2019-03-27 20:01:54 +0000

    Up to part 2 complete, pc crashing with hyper-v enabled to have to switch to laptop to finish lab.

--- worksheets/3-graphSQL/index.js
+++ worksheets/3-graphSQL/index.js
@@ -3,58 +3,16 @@ const { GraphQLServer } = require('graphql-yoga')
 
 const resolvers = {
   Query: {
-    publishedPosts(root, args, context) {
-      return context.prisma.posts({ where: { published: true } })
-    },
-    post(root, args, context) {
-      return context.prisma.post({ id: args.postId })
-    },
-    postsByUser(root, args, context) {
-      return context.prisma.user({
-        id: args.userId
-      }).posts()
+    product(root, args, context) {
+      return context.prisma.product({ id: args.productId })
     }
   },
   Mutation: {
-    createDraft(root, args, context) {
-      return context.prisma.createPost(
-        {
-          title: args.title,
-          author: {
-            connect: { id: args.userId }
-          }
-        },
-
-      )
+    createCategory(root, args, context) {
+      return context.prisma.createCategory(
+        { categoryname: args.categoryname },
+        )
     },
-    publish(root, args, context) {
-      return context.prisma.updatePost(
-        {
-          where: { id: args.postId },
-          data: { published: true },
-        },
-
-      )
-    },
-    createUser(root, args, context) {
-      return context.prisma.createUser(
-        { name: args.name },
-      )
-    }
-  },
-  User: {
-    posts(root, args, context) {
-      return context.prisma.user({
-        id: root.id
-      }).posts()
-    }
-  },
-  Post: {
-    author(root, args, context) {
-      return context.prisma.post({
-        id: root.id
-      }).author()
-    }
   }
 }
 
@@ -65,4 +23,5 @@ const server = new GraphQLServer({
     prisma
   },
 })
+
 server.start(() => console.log('Server is running on http://localhost:4000'))
